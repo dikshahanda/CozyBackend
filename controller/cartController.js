c934c0046445c2bd16e942532fbd8d21c67e978e
Narayan Jha <wakeupcoders@gmail.com>
2024-08-30 13:32:03 +0530

    Cart Decrease feature also added.

--- controller/cartController.js
+++ controller/cartController.js
@@ -53,5 +53,53 @@ const cart = async (req, res, next)=>{
     }
 }
 
-module.exports = {cart}
+const decreaseCart = async (req, res, next)=>{
+    try{
+        const userId = req.body.userId;
+        let user = await userSchema.findOne({ _id: userId });
+
+        if (!user) {
+            return res
+                .status(400)
+                .send({ status: false, message: "Invalid user ID" });
+        }
+
+        let productId = req.body.productId;
+        let product = await productSchema.findOne({ _id: productId });
+        if (!product) {
+            return res
+                .status(400)
+                .send({ status: false, message: "Invalid product" });
+        }
+
+        let cart = await Cart.findOne({ userId: userId });
+
+        if (!cart) {
+            return res.status(200).json({ status: false, message: "Cart not found for this user", cart: [], });
+        }
+
+
+        let itemIndex = cart.products.findIndex((p) => p.productId == productId);
+
+        if (itemIndex > -1) {
+            let productItem = cart.products[itemIndex];
+            if (productItem.quantity === 1) {
+                cart.products.splice(itemIndex, 1);
+                cart = await cart.save();
+                return res.status(200).send({ status: true, updatedCart: cart });
+            }
+            productItem.quantity -= 1;
+            cart.products[itemIndex] = productItem;
+            cart = await cart.save();
+            return res.status(200).send({ status: true, updatedCart: cart });
+        }
+        return res.status(400).json({ status: false, message: "Item does not exist in cart" });
+    }
+    catch(err){
+        console.log(err)
+        return res.status(500).json({'message':"Something went wrong"});
+    }
+}
+
+module.exports = {cart, decreaseCart}
 
