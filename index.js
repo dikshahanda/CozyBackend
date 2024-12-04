57a1f58c17fa849f56f87028ce4eac626f1362ee
Diksha <Dikshahanda2345@gmail.com>
2024-12-04 19:53:51 +0530

    dist folder added

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const cors = require('cors');
 const {router} = require('./routes/cozyRoutes');
 const {productrouter} = require('./routes/productRoutes');
 const {orderrouter} = require('./routes/orderRoutes');
-const {uploadroute} = require('./routes/uploadroutes')
-const {cartRouter} = require('./routes/cartRoutes')
-
+const {uploadroute} = require('./routes/uploadroutes');
+const {cartRouter} = require('./routes/cartRoutes');
+var path=require('path');
 
 const CozyDatabase = process.env.DATABASE;
 mongoose.connect(CozyDatabase);
@@ -25,7 +25,10 @@ app.use('/api/orders',orderrouter );
 app.use('/api/uploads', uploadroute);
 app.use('/api/cart', cartRouter);
  
-
+app.use(express.static(path.join(__dirname,'dist')));
+app.use("/*", function(req, res){
+    res.sendFile(path.join(__dirname+'/dist/index.html'))
+})
 
 app.listen(port, ()=>{
     console.log(`Server Started Successfully ${port}`)
